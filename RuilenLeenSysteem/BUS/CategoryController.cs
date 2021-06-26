51f6508f42847deeab6218ae3d83b30c342489b5
Max Berg <1966154berg@zuyd.nl>
2021-06-26 18:30:40 +0200

    overview kleine beetje aangepast

--- RuilenLeenSysteem/BUS/CategoryController.cs
+++ RuilenLeenSysteem/BUS/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RuilenLeenSysteem.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace RuilenLeenSysteem.BUS
 {
     class CategoryController
     {
+        internal List<Categorie> GetAllCategories()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- RuilenLeenSysteem/View/UserControls/Categorie/CategorieOverview.cs
+++ RuilenLeenSysteem/View/UserControls/Categorie/CategorieOverview.cs
@@ -7,15 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RuilenLeenSysteem.BUS;
+using RuilenLeenSysteem.Model;
 
 namespace RuilenLeenSysteem.View.UserControls.Categorie
 {
     public partial class CategorieOverview : UserControl
     {
+
+        private CategoryController _CategoryController;
         public CategorieOverview()
         {
             InitializeComponent();
+            _CategoryController = new CategoryController();
         }
+
+
 
         private void Btn_CategorieAdd_Click(object sender, EventArgs e)
         {
@@ -23,6 +30,35 @@ namespace RuilenLeenSysteem.View.UserControls.Categorie
             CategorieAdd Page = new CategorieAdd();
             Page.Dock = DockStyle.Fill;
             this.Controls.Add(Page);
+        }
+
+        private void CategorieOverview_Load(object sender, EventArgs e)
+        {
+            List<Model.Categorie> CategoryList = _CategoryController.GetAllCategories(); //afleiden van klanten
+
+            foreach (var Category in CategoryList)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(Dgv_CustomerOverview);
+                row.Cells[0].Value = Customer.Id;
+                row.Cells[1].Value = Customer.FirstName; //aanpassen naar category
+                row.Cells[2].Value = Customer.LastName;
+                row.Cells[3].Value = Customer.Email;
+                row.Cells[4].Value = Customer.BlackList;
+
+                Dgv_CustomerOverview.Rows.Add(row);
+
+            }
+        }
+
+        private void Dgv_CustomerOverview_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Dgv_CustomerOverview_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
